0df7ac23b50f865f5255ec17d539825b23b1d521
Alex Guillen <ag99u24@gmail.com>
2026-02-17 14:04:32 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
 import "bootstrap";
 import "./style.css";
-
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  
+  
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+
+  let numeros = 0
+  function numero(wh){
+    numeros = Math.floor(Math.random() * wh.length);
+    return numeros   
+  }
+
+  let a = who[numero(who)];
+  let b = action[numero(action)];
+  let c = what[numero(what)];
+  let d = when[numero(when)];
+
+  let excusa = a + " " + b + " " + c + "  "+ d;
+  
+  
+  document.getElementById("excuse").textContent = excusa;
+
+}
